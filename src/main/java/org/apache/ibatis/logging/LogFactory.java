5cfabba9860995a02cc21581cf4bf5ef954806ae
zhouze <ze.zhou@whats-egg.com>
2019-05-07 21:50:59 +0800

    log模块相关注释

--- src/main/java/org/apache/ibatis/logging/LogFactory.java
+++ src/main/java/org/apache/ibatis/logging/LogFactory.java
@@ -28,6 +28,9 @@ public final class LogFactory {
    */
   public static final String MARKER = "MYBATIS";
 
+  /**
+   * 记录当前使用的第三方日志组件所对应适配器的构造方法
+   */
   private static Constructor<? extends Log> logConstructor;
 
   static {
@@ -99,11 +102,13 @@ public final class LogFactory {
 
   private static void setImplementation(Class<? extends Log> implClass) {
     try {
+      //读取指定适配器的构造方法
       Constructor<? extends Log> candidate = implClass.getConstructor(String.class);
       Log log = candidate.newInstance(LogFactory.class.getName());
       if (log.isDebugEnabled()) {
         log.debug("Logging initialized using '" + implClass + "' adapter.");
       }
+      //初始化 logConstructor
       logConstructor = candidate;
     } catch (Throwable t) {
       throw new LogException("Error setting Log implementation.  Cause: " + t, t);
